7ebebc1db0837ad1e82cad0a7aa88d44db2594cd
Thibaud <thibaud.dubreucq@gmail.com>
2018-03-30 20:59:21 +0200

    Commit for merge

--- Server-Web-Api/labconsult/src/components/LoginComponent.js
+++ Server-Web-Api/labconsult/src/components/LoginComponent.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 import logo from '../logo.svg';
+import Button from 'material-ui/Button';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Input, { InputLabel } from 'material-ui/Input';
 import { FormControl, FormHelperText } from 'material-ui/Form';
-
-	const styles = theme => ({
+	
+	let boolzer = true;
+	const styles = theme => ({	
 	  grid: {
 		alignContent: 'center',
 	  },
@@ -26,6 +28,10 @@ import { FormControl, FormHelperText } from 'material-ui/Form';
 		textAlign: 'center',
 		color: theme.palette.text.secondary,
 	  },
+	  formLabel: {
+		  color: '#c6ebd6',
+		  borderColor: '#c6ebd6!important',
+	  }
 	});
 		
 	class LoginComponent extends React.Component {
@@ -47,21 +53,25 @@ import { FormControl, FormHelperText } from 'material-ui/Form';
 					  <h1 className="App-title">Lab Companion</h1>
 					</header>
 					<br/><br/><br/>
-					<Grid container spacing={12} align-content-xs-center>
+					<Grid container spacing={12} align="center">
 						<Grid item xs={4}/>
 						<Grid item xs={4}>
 							<Paper className={classes.paper}>
-							  <div className={classes.container}>
-								<FormControl className={classes.formControl}>
-								  <InputLabel htmlFor="name-simple">ID</InputLabel>
-								  <Input id="login" />
-								</FormControl>
-								<br/>
-								<FormControl className={classes.formControl}>
-								  <InputLabel htmlFor="name-simple">Password</InputLabel>
-								  <Input id="password" />
-								</FormControl>
-							  </div>
+								<Grid container spacing={12} justify="center" alignItems="center">
+								  <div className={classes.container}>
+									<FormControl className={classes.formControl}>
+									  <InputLabel htmlFor="name-simple">ID</InputLabel>
+									  <Input id="login" />
+									</FormControl>
+									<FormControl className={classes.formControl}>
+									  <InputLabel className={classes.formLabel} htmlFor="name-simple">Password</InputLabel>
+									  <Input className={classes.formLabel} id="password" />
+									</FormControl>
+								  </div>
+								</Grid>
+								<Grid container spacing={12} justify="center" alignItems="center">
+									<Button color="primary" className={classes.button}>Login</Button>
+								</Grid>
 							</Paper>
 						</Grid>
 					 </Grid>
